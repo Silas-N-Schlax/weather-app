87a8efd2f4fd725d1e0c39a996d993506ad848b8
SilasNS <silasschlax@gmail.com>
2026-01-15 14:17:10 -0500

    Added a fetch to get the weather data from a contoller and basic rendering for testing

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -6,7 +6,33 @@ export default class extends Controller {
     console.log("🔥 sidebar controller connected")
   }
 
-  select() {
+  async select(e) {
     console.log("clicked")
+
+    const locationID = e.currentTarget.id
+    try {
+      const response = await fetch('sidebar/get_weather_data', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': document.querySelector("meta[name='csrf-token']").content
+      },
+      body: JSON.stringify(locationID)
+    });
+    const result = await response.json()
+    console.log(result.data.daily);
+
+    document.querySelector(".main-content").innerHTML = `
+      <div class="weather-container">
+        <p>High: ${result.data.daily.temperature_2m_max[0]}</p>
+        <p>Low: ${result.data.daily.temperature_2m_min[0]}</p>
+      <div>
+    `
+
+    } catch (error) {
+      console.log(error)
+      //! render to user...
+    }
+    //! error handling, add try/catch
   }
 }
