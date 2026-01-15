d44fec359b01e73d9f0b20ca285bb1d86952f8e5
SilasNS <silasschlax@gmail.com>
2026-01-15 14:18:22 -0500

    Added my routes for sidebar controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   resources :locations
+  resources :sidebar, only: [ :index ] do
+    post :get_weather_data, on: :collection
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  post "/sidebar/get_weather_data", to: "sidebar#get_weather_data"
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
