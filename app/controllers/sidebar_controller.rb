ba13abdd639e5a42f6c0f78032471e5fafa78152
SilasNS <silasschlax@gmail.com>
2026-01-15 14:16:34 -0500

    Created controller stimulus connects to to get the weather data to stimulus.

--- app/controllers/sidebar_controller.rb
+++ app/controllers/sidebar_controller.rb
@@ -1,4 +1,22 @@
 class SidebarController < ApplicationController
-  def index
+  def get_weather_data
+    location_id = request.raw_post.split("_").last.to_i
+    location = Location.find(location_id)
+
+    weather_data = WeatherApi.new(location.lat, location.long)
+    result = weather_data.call
+
+    if result["error"]
+      puts result["reason"]
+      render json: {
+        error: true,
+        reason: result["reason"]
+      }
+      return
+    end
+    render json: {
+      error: false,
+      data: result["data"]
+    }
   end
 end
