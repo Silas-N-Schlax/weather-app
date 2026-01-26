106228383a19c7aa3ba8f82b978ffa3c98542fc3
SilasNS <silasschlax@gmail.com>
2026-01-26 09:43:38 -0500

    Added extra error checks to ensure proper errors are reaching the end user for standard https status codes such as 429 (super common with this api).

--- app/services/ip_api.rb
+++ app/services/ip_api.rb
@@ -4,7 +4,20 @@ class IpApi
   end
 
   def call
+    error_codes = {
+      400 => "The request sent to the API was bad, please try again!",
+      404 => "The URL was Not Found, please try again or contact the developer for a fix!",
+      403 => "The API could not verify our identity, authentication has failed, please try again!",
+      405 => "The API received your request but has refused to respond please try again later!",
+      429 => "The API has rate limited you, please try again later!"
+    }
     response = HTTParty.get("https://ipapi.co/#{@ip}/json/")
+    if response.code != 200
+      return {
+        "error" => true,
+        "reason" => error_codes[response.code]
+      }
+    end
     stringified_json = JSON.parse(response.body)
     if stringified_json["error"]
       {
